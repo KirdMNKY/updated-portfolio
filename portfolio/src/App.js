a3739fe4ba7bfc1605bf9758c2343430c0197cd9
KirdMNKY <mr.mike.soto@gmail.com>
2018-10-26 08:20:23 -0500

    working routes and links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 import Title from './components/Title';
 import MainCard from './components/MainCard';
 import AboutCard from './components/AboutCard';
-import PortfolioCard from './components/PortfolioCard';
+import PortfolioCard from './components/PortfolioCard/PortfolioCard';
 
 import Footer from './components/Footer';
 
@@ -17,24 +17,26 @@ import Footer from './components/Footer';
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className="main">
         <Title />
         <div className="wrapper">
           <MainCard />
-          <Router>
-          <Switch>
-                    <Route exact path="/" component={AboutCard} />
-                    <Route exact path="/About" component={AboutCard} />
-                    <Route exact path="/Portfolio" component={PortfolioCard} />
-                    {/* <Route exact path="/Blog" component={Blog} />
-                    <Route path="/Contact" component={Contact} />  */}
-                </Switch>
-          </Router>
-          <AboutCard />
-          <PortfolioCard />
+          
+
+              <Route exact path="/" component={AboutCard} />
+              <Route exact path="/About" component={AboutCard} />
+              <Route exact path="/Portfolio" component={PortfolioCard} />
+              {/* <Route exact path="/Blog" component={Blog} />
+              <Route path="/Contact" component={Contact} />  */}
+
+          
+          {/* <AboutCard />
+          <PortfolioCard /> */}
         </div>
         <Footer />
       </div>
+      </Router>
     );
   }
 }
